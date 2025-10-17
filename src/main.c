7a52456e78221b6c5ab49f556827c735b64aa443
Higor Lima <limahigor.g@gmail.com>
2025-10-17 19:35:06 -0300

    feat: add task03 code

--- src/main.c
+++ src/main.c
@@ -1,157 +1,148 @@
-#include "zephyr/devicetree.h"
-#include "zephyr/drivers/gpio.h"
-#include "zephyr/init.h"
-#include "zephyr/sys/util.h"
-#include "zephyr/sys/util_macro.h"
-#include "zephyr/toolchain.h"
 #include <stdint.h>
-#include <stdio.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/pwm.h>
-#include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/printk.h>
 
-LOG_MODULE_REGISTER(hello_world);
+LOG_MODULE_REGISTER(threads_interact);
 
-#define FADE_STEPS 50
-
-enum MODE {
-	BLINK = 0,
-	FADE
+enum TYPE {
+	TEMP = 0,
+	HUMI
 };
 
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(DT_NODELABEL(button0), gpios);
-static const struct pwm_dt_spec led = PWM_DT_SPEC_GET(DT_ALIAS(led0));
-static struct gpio_callback cb_data;
+struct data {
+	enum TYPE type;
+	int8_t item;
+};
 
-struct k_timer blink_timer;
-struct k_timer fade_timer;
+K_MSGQ_DEFINE(consumer_queue, sizeof(struct data), 10, 4);
+K_MSGQ_DEFINE(producer_queue, sizeof(struct data), 10, 4);
 
-static const struct device *const console_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-
-void blink(struct k_timer *timer_id)
+int8_t get_temp()
 {
-	static bool led_status = 1;
-
-	if (led_status) {
-		LOG_DBG("Aceso!");
-		pwm_set_pulse_dt(&led, led.period);
-	} else {
-		LOG_DBG("Apagado!");
-		pwm_set_pulse_dt(&led, 0);
-	}
-
-	led_status = !led_status;
+	return (k_cycle_get_32() % 31) + 10;
 }
 
-void fade(struct k_timer *timer_id)
+int8_t get_humi()
 {
-	static int8_t dir = 1;
-	static int32_t bright = 0;
-	static int32_t actual_step = 0;
+	return k_cycle_get_32() % 101;
+}
 
-	bright += (led.period / FADE_STEPS) * dir;
+void prod_thread(void *arg1, void *arg2, void *arg3)
+{
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
 
-	if (bright > led.period) {
-		bright = led.period;
+	enum TYPE type = (enum TYPE)(uintptr_t)arg1;
+
+	struct data data;
+	while (true) {
+		uint32_t temp_value;
+		char *s;
+
+		switch (type) {
+		case TEMP:
+			temp_value = get_temp();
+			s = "TEMP";
+
+			break;
+		case HUMI:
+			temp_value = get_humi();
+			s = "HUMI";
+
+			break;
+		default:
+			return;
+		}
+
+		LOG_DBG("Put [%s:%d] on queue...\n", s, temp_value);
+
+		data.item = temp_value;
+		data.type = type;
+
+		k_msgq_put(&producer_queue, &data, K_NO_WAIT);
+		k_sleep(K_SECONDS(1));
 	}
 
-	pwm_set_pulse_dt(&led, bright);
+	return;
+}
 
-	bright = MIN(led.period, MAX(0, bright));
+void filter_thread(void *arg1, void *arg2, void *arg3)
+{
+	ARG_UNUSED(arg1);
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
 
-	LOG_DBG("PWM Bright: %d%%", (bright * 100) / led.period);
+	struct data data;
+	while (true) {
+		int err = k_msgq_get(&producer_queue, &data, K_FOREVER);
+		if (err != 0) {
+			continue;
+		}
 
-	if (actual_step >= FADE_STEPS) {
-		actual_step = 0;
-		dir *= -1;
-	} else {
-		actual_step++;
+		char *s;
+
+		switch (data.type) {
+		case TEMP:
+			s = "TEMP";
+
+			if (data.item < 18 || data.item > 30) {
+				LOG_ERR("Removed invalid data for [%s:%d] from the queue\n", s,
+					data.item);
+				continue;
+			}
+
+			break;
+		case HUMI:
+			s = "HUMI";
+
+			if (data.item < 40 || data.item > 70) {
+				LOG_ERR("Removed invalid data for [%s:%d] from the queue\n", s,
+					data.item);
+				continue;
+			}
+
+			break;
+		default:
+			continue;
+		}
+
+		LOG_DBG("Filtered [%s:%d] from queue\n", s, data.item);
+		k_msgq_put(&consumer_queue, &data, K_NO_WAIT);
 	}
 }
 
-void button_cb(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+void consu_thread(void *arg1, void *arg2, void *arg3)
 {
-	ARG_UNUSED(dev);
-	ARG_UNUSED(cb);
-	ARG_UNUSED(pins);
+	struct data data;
+	while (true) {
+		int err = k_msgq_get(&consumer_queue, &data, K_FOREVER);
+		if (err != 0) {
+			continue;
+		}
 
-	static enum MODE mode = 0;
+		switch (data.type) {
+		case TEMP:
+			LOG_INF("Temperature: %dºC\n", data.item);
 
-	switch (mode) {
-	case BLINK:
-		k_timer_stop(&blink_timer);
-		k_timer_start(&fade_timer, K_NO_WAIT, K_MSEC(20));
+			break;
+		case HUMI:
+			LOG_INF("Humidity: %d%%\n", data.item);
 
-		LOG_INF("BLINK -> FADE\n");
-		break;
-	case FADE:
-		k_timer_stop(&fade_timer);
-		k_timer_start(&blink_timer, K_NO_WAIT, K_MSEC(CONFIG_BLINK_TIMER_INTERVAL));
-
-		LOG_INF("FADE -> BLINK\n");
-		break;
+			break;
+		default:
+			continue;
+		}
 	}
-
-	mode = !mode;
-	k_msleep(20);
 }
 
 int main(void)
 {
-	int ret = 0;
-	LOG_INF("Starting system....\n");
-
-	if (!gpio_is_ready_dt(&button)) {
-		printk("Error: button device %s is not ready!", button.port->name);
-
-		return 0;
-	}
-
-	if (!pwm_is_ready_dt(&led)) {
-		printk("Error: PWM device %s is not ready\n", led.dev->name);
-		return 0;
-	}
-
-	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret != 0) {
-		printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name,
-		       button.pin);
-
-		return 0;
-	}
-
-	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-	if (ret != 0) {
-		printk("Error: failed to to configure intrreupt on %s pin %d!", button.port->name,
-		       button.pin);
-
-		return 0;
-	}
-
-	gpio_init_callback(&cb_data, button_cb, BIT(button.pin));
-	gpio_add_callback(button.port, &cb_data);
-
-	k_sleep(K_SECONDS(1));
-
-	LOG_INF("Starting blink....\n");
-	LOG_INF("Press button to toggle mode anytime!\n");
-
-	k_timer_init(&fade_timer, fade, NULL);
-	k_timer_init(&blink_timer, blink, NULL);
-	k_timer_start(&blink_timer, K_NO_WAIT, K_MSEC(CONFIG_BLINK_TIMER_INTERVAL));
-
-	char c;
-	while (1) {
-		if (!uart_poll_in(console_dev, &c) && (c == '\n' || c == '\r')) {
-			LOG_DBG("Button Pressed!\n");
-			button_cb(button.port, &cb_data, button.pin);
-		}
-
-		k_msleep(50);
-	}
-
 	return 0;
 }
+
+K_THREAD_DEFINE(producer1_thread, 1024, prod_thread, TEMP, NULL, NULL, 4, 0, 0);
+K_THREAD_DEFINE(producer2_thread, 1024, prod_thread, HUMI, NULL, NULL, 4, 0, 0);
+K_THREAD_DEFINE(filtering_thread, 1024, filter_thread, NULL, NULL, NULL, 3, 0, 0);
+K_THREAD_DEFINE(consulmer_thread, 1024, consu_thread, NULL, NULL, NULL, 2, 0, 0);
